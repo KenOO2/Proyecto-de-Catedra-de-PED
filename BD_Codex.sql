a2966ebf0da8ddfeac52d102c6c44e2ff7db5d65
KenOO2 <146603328+KenOO2@users.noreply.github.com>
2025-04-29 16:32:53 -0600

    Base de datos para el funcionamiento del programa

---
+++ BD_Codex.sql
@@ -0,0 +1,44 @@
+use Codex
+
+CREATE DATABASE Codex
+
+CREATE TABLE Usuarios(
+Id int PRIMARY KEY NOT NULL,
+Email varchar(30) NOT NULL,
+Nombre varchar(10) NOT NULL,
+Contraseña varchar(8) NOT NULL
+);
+
+CREATE TABLE Proveedores(
+IdProveedor int PRIMARY KEY,
+NomMarca varchar(15) NOT NULL,
+Email varchar(30) NOT NULL,
+TelProveedor char(16),
+RegistroIva char(10) NOT NULL
+);
+
+
+CREATE TABLE Productos(
+IdProducto CHAR(10) PRIMARY KEY,
+Nomproducto varchar(30) NOT NULL,
+IdProveedor int NOT NULL,
+Precio decimal(10,2) NOT NULL,
+Descuento int NOT NULL, --Entrada esperada:10, 50, 99 {(10/100) = 0.1 * precio = descuento }
+FOREIGN KEY (IdProveedor) REFERENCES Proveedores(IdProveedor) ON DELETE CASCADE
+);
+
+
+insert into Usuarios(id, Email, Nombre, Contraseña)
+values ('1','gab@gmail.com', 'Gabriel', '1234')
+
+insert into Proveedores(IdProveedor, NomMarca, Email, TelProveedor, RegistroIva)
+values ('1','adoc', 'pp@gmail.com', '121254545', '3')
+
+insert into Productos(IdProducto, Nomproducto, IdProveedor, Precio, Descuento)
+values ('1','Zapato deporte', '1', '12.8', '2')
+
+
+select * from Usuarios
+select * from Productos
+select * from Proveedores
+
